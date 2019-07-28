89101d0e39633256d8501721bed116fa2419a09b
Selvaraj <ms12122@lehmann12.skb.med.nyu.edu>
2019-07-28 16:35:44 -0400

    Running hdbscan on optimal min cluster size

--- HDBSCAN.py
+++ HDBSCAN.py
@@ -58,12 +58,12 @@ coordinates = getPoints('3DCoordinates.csv')
 #coordinateVectors is the input to clustering algorithms 
 coordinateVectors = np.vstack((coordinates[0],coordinates[1],coordinates[2])).T
 
-#HDBSCAN
+#Iterative HDBSCAN to determine best parameter
 minClusterSize = list() #list to store the min cluster size for plotting
 numP = list() #list to store the number of particles/clusters identified
 sil = list() #list to store the corresponding silhouette score
 #Running HDBSCAN for less than 50nm radius(min cluster size = 3) to 450nm radius (min cluster size = 56)
-for i in range(3,57,1):
+for i in range(3,5,1):
     print(i)
     minClusterSize.append(i)
     hdbscanObj = hdbscan.HDBSCAN(min_cluster_size=i)
@@ -75,14 +75,32 @@ for i in range(3,57,1):
     sil.append(HDBSCANSilhouette)
     #print("HDBSCAN Silhouette score: " + str(HDBSCANSilhouette))
 
-plt.plot(minClusterSize,numP)
-plt.plot(minClusterSize,sil)
-plt.xlabel("hdbscan min cluster size")
+# Two subplots sharing X
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(minClusterSize,numP)
+#axarr[0].set_xlabel('hdbscan min cluster size')
+axarr[0].set_ylabel('number of granules identified')
+axarr[1].plot(minClusterSize,sil)
+axarr[1].set_xlabel('hdbscan min cluster size')
+axarr[1].set_ylabel('silhouette score')
 plt.show()
-'''
+
+#Getting the index of ma silhouette score
+maxIndex = np.argmax(sil)
+optMinClusterSize = minClusterSize[maxIndex]
+print("Optimal min cluster size: " + str(optMinClusterSize)) 
+
+#Running HDBSCAN on the optimal min cluster size
+hdbscanObj = hdbscan.HDBSCAN(min_cluster_size=optMinClusterSize)
+optlabels = hdbscanObj.fit_predict(coordinateVectors)
+optnumParticles = max(optlabels) + 1 #Adding one because zero is a label
+print("Number of germ plasm RNPs identified: " + str(optnumParticles)) 
+optHDBSCANSilhouette = sklearn.metrics.silhouette_score(coordinateVectors, optlabels)
+print("HDBSCAN Silhouette score: " + str(optHDBSCANSilhouette))
+
+
 #Visualization 
 #Generating a random list of colors for each label 
-colors = generateColors(numParticles, labels)
+colors = generateColors(optnumParticles, optlabels)
 #Creating a widget to view the clusters
 createWidget(coordinateVectors,colors)
-'''
