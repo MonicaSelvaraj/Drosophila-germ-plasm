27ce88b288e17f6130c3de9b7593b11c2a8489de
Selvaraj <ms12122@lehmann12.skb.med.nyu.edu>
2019-07-31 12:12:07 -0400

    Changed lower limit on min cluster size to 4 to have min 4 pts in 3D to compute the convex hull

--- HDBSCAN.py
+++ HDBSCAN.py
@@ -63,7 +63,7 @@ minClusterSize = list() #list to store the min cluster size for plotting
 numP = list() #list to store the number of particles/clusters identified
 sil = list() #list to store the corresponding silhouette score
 #Running HDBSCAN for less than 50nm radius(min cluster size = 3) to 450nm radius (min cluster size = 56)
-for i in range(3,57,1):
+for i in range(4,57,1):
     print(i)
     minClusterSize.append(i)
     hdbscanObj = hdbscan.HDBSCAN(min_cluster_size=i)
